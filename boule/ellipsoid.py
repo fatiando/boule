60c37b8169d85351398a3892c7a60b8ae182d788
Leonardo Uieda <leouieda@gmail.com>
2019-11-29 16:49:29 +0000

    Spherical to geodetic and inverse transformations
    Add methods to Ellipsoid to transform coordinates from geodetic to
    spherical and back again. Copied from Harmonica. Added as methods to
    avoid passing "ellipsoid" all the time or having to use a global
    ellipsoid.

--- boule/ellipsoid.py
+++ boule/ellipsoid.py
@@ -145,3 +145,105 @@ class Ellipsoid:
             self.geocentric_grav_const * (1 + self.emm * aux) / self.semimajor_axis ** 2
         )
         return result
+
+    def geodetic_to_spherical(self, longitude, latitude, height):
+        """
+        Convert from geodetic to geocentric spherical coordinates.
+
+        The geodetic datum is defined by this ellipsoid. The coordinates are
+        converted following [Vermeille2002]_.
+
+        Parameters
+        ----------
+        longitude : array
+            Longitude coordinates on geodetic coordinate system in degrees.
+        latitude : array
+            Latitude coordinates on geodetic coordinate system in degrees.
+        height : array
+            Ellipsoidal heights in meters.
+
+        Returns
+        -------
+        longitude : array
+            Longitude coordinates on geocentric spherical coordinate system in
+            degrees.
+            The longitude coordinates are not modified during this conversion.
+        spherical_latitude : array
+            Converted latitude coordinates on geocentric spherical coordinate
+            system in degrees.
+        radius : array
+            Converted spherical radius coordinates in meters.
+
+        """
+        latitude_rad = np.radians(latitude)
+        prime_vertical_radius = self.semimajor_axis / np.sqrt(
+            1 - self.first_eccentricity ** 2 * np.sin(latitude_rad) ** 2
+        )
+        # Instead of computing X and Y, we only compute the projection on the
+        # XY plane: xy_projection = sqrt( X**2 + Y**2 )
+        xy_projection = (height + prime_vertical_radius) * np.cos(latitude_rad)
+        z_cartesian = (
+            height + (1 - self.first_eccentricity ** 2) * prime_vertical_radius
+        ) * np.sin(latitude_rad)
+        radius = np.sqrt(xy_projection ** 2 + z_cartesian ** 2)
+        spherical_latitude = np.degrees(np.arcsin(z_cartesian / radius))
+        return longitude, spherical_latitude, radius
+
+    def spherical_to_geodetic(self, longitude, spherical_latitude, radius):
+        """
+        Convert from geocentric spherical to geodetic coordinates.
+
+        The geodetic datum is defined by this ellipsoid. The coordinates are
+        converted following [Vermeille2002]_.
+
+        Parameters
+        ----------
+        longitude : array
+            Longitude coordinates on geocentric spherical coordinate system in
+            degrees.
+        spherical_latitude : array
+            Latitude coordinates on geocentric spherical coordinate system in
+            degrees.
+        radius : array
+            Spherical radius coordinates in meters.
+
+        Returns
+        -------
+        longitude : array
+            Longitude coordinates on geodetic coordinate system in degrees.
+            The longitude coordinates are not modified during this conversion.
+        latitude : array
+            Converted latitude coordinates on geodetic coordinate system in
+            degrees.
+        height : array
+            Converted ellipsoidal height coordinates in meters.
+
+        """
+        spherical_latitude_rad = np.radians(spherical_latitude)
+        big_z = radius * np.sin(spherical_latitude_rad)
+        p_0 = (
+            radius ** 2 * np.cos(spherical_latitude_rad) ** 2 / self.semimajor_axis ** 2
+        )
+        q_0 = (1 - self.first_eccentricity ** 2) / self.semimajor_axis ** 2 * big_z ** 2
+        r_0 = (p_0 + q_0 - self.first_eccentricity ** 4) / 6
+        s_0 = self.first_eccentricity ** 4 * p_0 * q_0 / 4 / r_0 ** 3
+        t_0 = np.cbrt(1 + s_0 + np.sqrt(2 * s_0 + s_0 ** 2))
+        u_0 = r_0 * (1 + t_0 + 1 / t_0)
+        v_0 = np.sqrt(u_0 ** 2 + q_0 * self.first_eccentricity ** 4)
+        w_0 = self.first_eccentricity ** 2 * (u_0 + v_0 - q_0) / 2 / v_0
+        k = np.sqrt(u_0 + v_0 + w_0 ** 2) - w_0
+        big_d = (
+            k
+            * radius
+            * np.cos(spherical_latitude_rad)
+            / (k + self.first_eccentricity ** 2)
+        )
+        latitude = np.degrees(
+            2 * np.arctan(big_z / (big_d + np.sqrt(big_d ** 2 + big_z ** 2)))
+        )
+        height = (
+            (k + self.first_eccentricity ** 2 - 1)
+            / k
+            * np.sqrt(big_d ** 2 + big_z ** 2)
+        )
+        return longitude, latitude, height
