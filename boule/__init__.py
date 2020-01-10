1ed0e1fd6f719ff09d43bd2863e622c74fc66dcc
Leonardo Uieda <leouieda@gmail.com>
2020-01-10 10:37:47 +0000

    Add an ellipsoid for Mars (#15)
    Based on the numbers is Ardalan et al. (2009). Include a plot of normal
    gravity on Mars in the tutorial as well.

--- boule/__init__.py
+++ boule/__init__.py
@@ -2,10 +2,10 @@
 # Import functions/classes to make the public API
 from . import version
 from .ellipsoid import Ellipsoid
-from .realizations import WGS84, GRS80
+from .realizations import WGS84, GRS80, MARS
 
 
-ELLIPSOIDS = [WGS84, GRS80]
+ELLIPSOIDS = [WGS84, GRS80, MARS]
 
 
 def test(doctest=True, verbose=True):
