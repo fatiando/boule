8c33a537ae5abcf81e826c371264f49f329c3119
Leonardo Uieda <leouieda@gmail.com>
2020-10-14 14:30:27 +0100

    Add a serve option to the Makefile

--- Makefile
+++ Makefile
@@ -4,11 +4,13 @@ PROJECT=boule
 help:
 	@echo "Commands:"
 	@echo ""
-	@echo "  install   install in editable mode"
-	@echo "  test      run the test suite (including doctests) and report coverage"
 	@echo "  format    run black to automatically format the code"
+	@echo "  install   install in editable mode"
 	@echo "  check     run code style and quality checks (black and flake8)"
 	@echo "  lint      run pylint for a deeper (and slower) quality check"
+	@echo "  test      run the test suite (including doctests) and report coverage"
+	@echo "  docs      build the documentation"
+	@echo "  serve     serve the documentation at http://localhost:8000"
 	@echo "  clean     clean up build and generated files"
 	@echo ""
 
@@ -35,13 +37,22 @@ lint:
 docs:
 	nox -s docs
 
+serve: docs
+	cd doc/_build/html && python -m http.server 8000
+
 clean:
 	find $(PROJECT) -name "*.pyc" -exec rm -v {} \;
 	find . -name ".coverage.*" -exec rm -v {} \;
-	rm -rvf build dist MANIFEST *.egg-info __pycache__ .coverage .cache .pytest_cache
-	rm -rf doc/_build/
-	rm -rf doc/api/generated
-	rm -rf doc/gallery
-	rm -rf doc/tutorials
-	rm -rf doc/sample_data
-	rm -rf doc/.ipynb_checkpoints
+	rm -rvf build dist \
+		MANIFEST \
+		*.egg-info \
+		__pycache__ \
+		.coverage \
+		.cache \
+		.pytest_cache \
+		doc/_build/ \
+		doc/api/generated \
+		doc/gallery \
+		doc/tutorials \
+		doc/sample_data \
+		doc/.ipynb_checkpoints
--- noxfile.py
+++ noxfile.py
@@ -11,7 +11,7 @@ PACKAGE = "boule"
 PYTHON_VERSIONS = ["3.8", "3.7", "3.6"]
 PYLINT_FILES = [PACKAGE, "setup.py"]
 FLAKE8_FILES = [PACKAGE, "setup.py", "doc/conf.py"]
-BLACK_FILES = [PACKAGE, "setup.py", "doc/conf.py", "tutorials"]
+BLACK_FILES = [PACKAGE, "setup.py", "doc/conf.py", "tutorials", "noxfile.py"]
 PYTEST_ARGS = [
     "--cov-config=.coveragerc",
     "--cov-report=term-missing",
@@ -29,16 +29,7 @@ nox.options.reuse_existing_virtualenvs = True
 nox.options.sessions = ("black_check", "flake8", "pylint", "test")
 
 
-@nox.session(python=["3.7"])
-def format(session):
-    """
-    Run black to format the code
-    """
-    session.install("black")
-    session.run("black", *BLACK_FILES)
-
-
-@nox.session(python=["3.7"])
+@nox.session()
 def black_check(session):
     """
     Check code style using black
@@ -47,7 +38,7 @@ def black_check(session):
     session.run("black", "--check", *BLACK_FILES)
 
 
-@nox.session(python=["3.7"])
+@nox.session()
 def flake8(session):
     """
     Check code style using flake8
@@ -56,7 +47,7 @@ def flake8(session):
     session.run("flake8", *FLAKE8_FILES)
 
 
-@nox.session(python=["3.7"])
+@nox.session()
 def pylint(session):
     """
     Check code style using pylint
@@ -65,32 +56,19 @@ def pylint(session):
     session.run("pylint", *PYLINT_FILES)
 
 
-@nox.session(python=["3.7"])
-def docs(session):
+@nox.session()
+def format(session):
     """
-    Build the documentation pages
+    Run black to format the code
     """
-    session.install("-r", "env/requirements-docs.txt", "-r", "requirements.txt")
-    # Install the package
-    session.install("--no-deps", "-e", ".")
-    # Generate the API reference
-    session.run(
-        "sphinx-autogen",
-        "-i",
-        "-t",
-        "doc/_templates",
-        "-o",
-        "doc/api/generated",
-        "doc/api/index.rst",
-    )
-    # Build the HTML pages
-    session.run("sphinx-build", "-d", "doc/_build/doctrees", "doc", "doc/_build")
+    session.install("black")
+    session.run("black", *BLACK_FILES)
 
 
 @nox.session(python=PYTHON_VERSIONS)
 def test(session):
     """
-    Run the test and measure coverage (using pip)
+    Run the tests and measure coverage (using pip)
     """
     session.install("-r", "env/requirements-test.txt", "-r", "requirements.txt")
     # Install the package
@@ -109,3 +87,25 @@ def test_conda(session):
     # Install the package
     session.install("--no-deps", "-e", ".")
     session.run("pytest", *PYTEST_ARGS, PACKAGE, *RST_FILES)
+
+
+@nox.session()
+def docs(session):
+    """
+    Build the documentation
+    """
+    session.install("-r", "env/requirements-docs.txt", "-r", "requirements.txt")
+    # Install the package
+    session.install("--no-deps", "-e", ".")
+    # Generate the API reference
+    session.run(
+        "sphinx-autogen",
+        "-i",
+        "-t",
+        "doc/_templates",
+        "-o",
+        "doc/api/generated",
+        "doc/api/index.rst",
+    )
+    # Build the HTML pages
+    session.run("sphinx-build", "-d", "doc/_build/doctrees", "doc", "doc/_build/html")
