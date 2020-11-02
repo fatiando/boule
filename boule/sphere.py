b109dfb380ea4210a29e3013adfde409b2a54870
Santiago Soler <santiago.r.soler@gmail.com>
2020-11-02 11:20:32 -0300

    Revert "Add license and copyright notice to every .py file"
    This reverts commit 7fa861f218d7ed29456190b5df58db77d6f4a332.

--- boule/sphere.py
+++ boule/sphere.py
@@ -1,8 +1,3 @@
-# Copyright (c) YEAR The PROJECT Developers.
-# Distributed under the terms of the BSD 3-Clause License.
-# SPDX-License-Identifier: BSD-3-Clause
-#
-# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 """
 Define the reference sphere (ellipsoid with 0 flattening).
 """
