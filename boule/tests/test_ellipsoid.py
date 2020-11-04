4e5a8269257e8cc1e1ec74f23dbdb56061b96f20
Santiago Soler <santiago.r.soler@gmail.com>
2020-11-04 09:05:30 -0300

    Revert "Add license and copyright notice to every .py file"
    This reverts commit c7135db40daf102be9e289ffcd4d6e1d640cacee.

--- boule/tests/test_ellipsoid.py
+++ boule/tests/test_ellipsoid.py
@@ -1,8 +1,3 @@
-# Copyright (c) 2019 The Boule Developers.
-# Distributed under the terms of the BSD 3-Clause License.
-# SPDX-License-Identifier: BSD-3-Clause
-#
-# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 # pylint: disable=redefined-outer-name
 """
 Test the base Ellipsoid class.
