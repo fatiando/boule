42103c1a42562ce0bc0efbdef81dda0b8b2824c9
Santiago Soler <santiago.r.soler@gmail.com>
2020-11-09 10:50:57 -0300

    Add license and copyright notice to every .py file (#67)
    Related to fatiando/maintenance#10

--- boule/sphere.py
+++ boule/sphere.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2019 The Boule Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
 """
 Define the reference sphere (ellipsoid with 0 flattening).
 """
