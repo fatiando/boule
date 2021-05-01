a0a22394281e99f2aecb44e158c24f527bf55dc6
M. G <mariana22@ciencias.unam.mx>
2021-05-01 02:17:02 -0700

    Check where normal gravity is computed
    Check if normal gravity is computed on internal point #44.
    Use np.linalg.norm(height) because height can be an array.

--- boule/ellipsoid.py
+++ boule/ellipsoid.py
@@ -453,7 +453,7 @@ class Ellipsoid:
             The normal gravity in mGal.
 
         """
-        if height < 0:
+        if np.linalg.norm(height) < 0:
             warn(
                 "Formulas used are valid for points outside the ellipsoid."
                 "Height must be major or equal to zero."
