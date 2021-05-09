8b489259f23dcd9231546ae0af1f5df582a213cb
M. G <mariana22@ciencias.unam.mx>
2021-05-09 11:33:52 -0700

    Warn if height is negative
    Use np.any(height < 0)

--- boule/ellipsoid.py
+++ boule/ellipsoid.py
@@ -453,7 +453,8 @@ class Ellipsoid:
             The normal gravity in mGal.
 
         """
-        if np.any(height) == True:
+        # Warn if height is negative
+        if np.any(height < 0):
             warn(
                 "Formulas used are valid for points outside the ellipsoid."
                 "Height must be major or equal to zero."
