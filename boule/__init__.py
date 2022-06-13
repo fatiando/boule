bdd562fd144ca7d350ca03bf9cf07001bc2e7c3b
Leonardo Uieda <leouieda@gmail.com>
2022-06-13 11:49:24 +0100

    Run black to fix format

--- boule/__init__.py
+++ boule/__init__.py
@@ -5,11 +5,11 @@
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
 # Import functions/classes to make the public API
+from ._version import __version__
 from .ellipsoid import Ellipsoid
 from .realizations import GRS80, MARS, MERCURY, MOON, VENUS, VESTA, WGS84
 from .sphere import Sphere
 from .triaxialellipsoid import TriaxialEllipsoid
-from ._version import __version__
 
 ELLIPSOIDS = [WGS84, GRS80, MARS, VENUS, MOON, MERCURY]
 
