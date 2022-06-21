abc10867c8b611cd2db1ea11afad73237917efa2
Leonardo Uieda <leouieda@gmail.com>
2022-06-21 22:01:10 +0100

    Refactor documentation of TriaxialEllipsoid
    Update the docstrings to match the new format of Ellipsoid. Includes
    better maths rendering and uses the Vesta ellipsoid as the example.

--- boule/_triaxialellipsoid.py
+++ boule/_triaxialellipsoid.py
@@ -5,7 +5,7 @@
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
 """
-Module for defining and setting the reference ellipsoid.
+Define a reference triaxial ellipsoid.
 """
 from warnings import warn
 
@@ -17,47 +17,50 @@ import numpy as np
 # accidentally.
 @attr.s(frozen=True)
 class TriaxialEllipsoid:
-    """
-    Reference triaxial ellipsoid.
+    r"""
+    A rotating triaxial ellipsoid.
 
-    The ellipsoid is triaxial and spins around it's largest moment of inertia.
-    It is defined by five parameters (semi-major axis, semi-medium axis,
-    semi-minor axis,  geocentric gravitational constant, and angular velocity)
-    and offers other derived quantities.
+    The ellipsoid is defined by five parameters: semimajor axis, semimedium
+    axis, semiminor axis, geocentric gravitational constant, and angular
+    velocity The thee semi-axis are different and the ellipsoid spins around
+    it's largest moment of inertia.
 
-    **All attributes of this class are read-only and cannot be changed after
-    instantiation.**
+    **This class is read-only:** Input parameters and attributes cannot be
+    changed after instantiation.
 
-    All parameters are in SI units.
+    **Units:** All input parameters and derived attributes are in SI units.
 
-    .. note::
-
-        Use :class:`boule.Sphere` if you desire zero flattening or
-        :class:`boule.Ellipsoid` for oblate ellipsoids. Some calculations would
-        not work by setting the axis to the same value.
-
-    .. warning::
+    .. attention::
 
         Gravity calculations have not been implemented yet for triaxial
-        ellipsoids.
+        ellipsoids. If you're interested in this feature or would like to help
+        implement, please `get in touch <https://www.fatiando.org/contact>`__.
 
     Parameters
     ----------
     name : str
-        A short name for the ellipsoid, for example ``'WGS84'``.
+        A short name for the ellipsoid, for example ``"WGS84"``.
     semimajor_axis : float
-        The semi-major axis of the ellipsoid, usually represented by "a"
-        [meters].
+        The semimajor (largest) axis of the ellipsoid.
+        Definition: :math:`a`.
+        Units: :math:`m`.
     semimedium_axis : float
-        The semi-medium axis of the ellipsoid, usually represented by "b"
-        [meters].
+        The semimedium (middle) axis of the ellipsoid.
+        Definition: :math:`b`.
+        Units: :math:`m`.
     semimajor_axis : float
-        The semi-minor axis of the ellipsoid, usually represented by "c"
-        [meters].
+        The semiminor (smallest) axis of the ellipsoid.
+        Definition: :math:`c`.
+        Units: :math:`m`.
     geocentric_grav_const : float
-        The geocentric gravitational constant (GM) [m^3 s^-2].
+        The geocentric gravitational constant. The product of the mass of the
+        ellipsoid :math:`M` and the gravitational constant :math:`G`.
+        Definition: :math:`GM`. Units:
+        :math:`m^3.s^{-2}`.
     angular_velocity : float
-        The angular velocity of the rotating ellipsoid (omega) [rad s^-1].
+        The angular velocity of the rotating ellipsoid.
+        Definition: :math:`\omega`.
+        Units: :math:`\\rad.s^{-1}`.
     long_name : str or None
         A long name for the ellipsoid, for example ``"World Geodetic System
         1984"`` (optional).
@@ -70,31 +73,32 @@ class TriaxialEllipsoid:
     We can define an ellipsoid by setting the 5 key numerical parameters:
 
     >>> ellipsoid = TriaxialEllipsoid(
-    ...     name="Watermelon",
-    ...     long_name="Large Watermelon",
-    ...     semimajor_axis=4,
-    ...     semimedium_axis=2,
-    ...     semiminor_axis=1,
-    ...     geocentric_grav_const=1,
-    ...     angular_velocity=0,
+    ...     name="VESTA",
+    ...     long_name="Vesta Triaxial Ellipsoid",
+    ...     semimajor_axis=286_300,
+    ...     semimedium_axis=278_600,
+    ...     semiminor_axis=223_200,
+    ...     geocentric_grav_const=1.729094e10,
+    ...     angular_velocity=326.71050958367e-6,
+    ...     reference=(
+    ...         "Russell, C. T., Raymond, C. A., Coradini, A., McSween, "
+    ...         "H. Y., Zuber, M. T., Nathues, A., et al. (2012). Dawn at "
+    ...         "Vesta: Testing the Protoplanetary Paradigm. Science. "
+    ...         "doi:10.1126/science.1219381"
+    ...     ),
     ... )
     >>> print(ellipsoid) # doctest: +ELLIPSIS
-    TriaxialEllipsoid(name='Watermelon', ...)
+    TriaxialEllipsoid(name='VESTA', ...)
     >>> print(ellipsoid.long_name)
-    Large Watermelon
+    Vesta Triaxial Ellipsoid
 
-    The class defines several derived attributes based on the input parameters:
+    The class then defines several derived attributes based on the input
+    parameters:
 
-    >>> print("{:.2f}".format(ellipsoid.semimajor_axis))
-    4.00
-    >>> print("{:.2f}".format(ellipsoid.semimedium_axis))
-    2.00
-    >>> print("{:.2f}".format(ellipsoid.semiminor_axis))
-    1.00
-    >>> print("{:.2f}".format(ellipsoid.mean_radius))
-    2.33
-    >>> print("{:.2f}".format(ellipsoid.volume))
-    33.51
+    >>> print(f"{ellipsoid.mean_radius:.0f} m")
+    262700 m
+    >>> print(f"{ellipsoid.volume * 1e-9:.0f} km³")
+    74573626 km³
 
     """
 
@@ -108,9 +112,7 @@ class TriaxialEllipsoid:
     reference = attr.ib(default=None)
 
     def _raise_invalid_axis(self):
-        """
-        Raise a ValueError informing that the axis are invalid.
-        """
+        "Raise a ValueError informing that the axis are invalid."
         raise ValueError(
             "Invalid triaxial ellipsoid axis: "
             f"major={self.semimajor_axis} "
@@ -122,7 +124,7 @@ class TriaxialEllipsoid:
     @semimajor_axis.validator
     def _check_semimajor_axis(self, semimajor_axis, value):
         """
-        Check if semimajor_axis is positive and is the largest of the radii
+        Check if semimajor_axis is positive and is the largest of the axis.
         """
         if not value > 0:
             raise ValueError(
@@ -134,21 +136,18 @@ class TriaxialEllipsoid:
     @semimedium_axis.validator
     def _check_semimedium_axis(self, semimedium_axis, value):
         """
-        Check if semimedium_axis is positive and larger than semi-minor axis
+        Check if semimedium_axis is positive and larger than semi-minor axis.
         """
         if not value > 0:
             raise ValueError(
                 f"Invalid semi-medium axis '{value}'. Should be greater than zero."
             )
-        # Check if semimedium axis is the middle of the 3 axes.
         if self.semiminor_axis > value:
             self._raise_invalid_axis()
 
     @semiminor_axis.validator
     def _check_semiminor_axis(self, semiminor_axis, value):
-        """
-        Check if semiminor_axis is positive
-        """
+        "Check if semiminor_axis is positive."
         if not value > 0:
             raise ValueError(
                 f"Invalid semi-minor axis '{value}'. Should be greater than zero."
@@ -158,25 +157,25 @@ class TriaxialEllipsoid:
 
     @geocentric_grav_const.validator
     def _check_geocentric_grav_const(self, geocentric_grav_const, value):
-        """
-        Warn if geocentric_grav_const is negative
-        """
+        "Warn if geocentric_grav_const is negative."
         if value < 0:
             warn(f"The geocentric gravitational constant is negative: '{value}'")
 
     @property
     def mean_radius(self):
+        r"""
+        The arithmetic mean radius of the ellipsoid.
+        Definition: :math:`R = \dfrac{a + b + c}{3}`.
+        Units: :math:`m`.
         """
-        The arithmetic mean radius :math:`R=(a+b+c)/3` [meters]
-        """
-        return (
-            1 / 3 * (self.semimajor_axis + self.semimedium_axis + self.semiminor_axis)
-        )
+        return (self.semimajor_axis + self.semimedium_axis + self.semiminor_axis) / 3
 
     @property
     def volume(self):
-        """
-        The volume :math:`V = /frac{4}{3} pi a b c` [meters^3]
+        r"""
+        The volume bounded by the ellipsoid.
+        Definition: :math:`V = \dfrac{4}{3} \pi a b c`.
+        Units: :math:`m^3`.
         """
         return (
             (4 / 3 * np.pi)
