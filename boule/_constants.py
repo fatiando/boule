58d673caf47d4a230d9c5fb9e039cc990af2dfe5
Mark Wieczorek <mark.wieczorek@ipgp.fr>
2024-03-25 22:43:26 +0100

    fix license

--- boule/_constants.py
+++ boule/_constants.py
@@ -3,15 +3,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
 """
 Gravitational constant
 units:  m3 / (kg s2)
 
-Reference:
-E. Tiesinga, P. J. Mohr, D. B. Newell, and B. N. Taylor (2019),
-"The 2018 CODATA Recommended Values of the Fundamental Physical Constants"
-(Web Version 8.1). Database developed by J. Baker, M. Douma, and S.
-Kotochigova. Available at http://physics.nist.gov/constants, National
-Institute of Standards and Technology, Gaithersburg, MD 20899.
+Reference
+---------
+E. Tiesinga, P. J. Mohr, D. B. Newell, and B. N. Taylor (2019), The 2018
+CODATA Recommended Values of the Fundamental Physical Constants (Web Version
+8.1). Database developed by J. Baker, M. Douma, and S. Kotochigova.
+Available at http://physics.nist.gov/constants, National Institute of
+Standards and Technology, Gaithersburg, MD 20899.
 """
 G = 6.67430e-11
