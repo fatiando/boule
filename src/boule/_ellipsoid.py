c3bc411772d52f3f96d6bd1613ca701cfa018a52
Leonardo Uieda <leo@uieda.com>
2026-03-25 14:10:46 -0300

    Add spherical to cartesian conversion for Ellipsoid (#234)
    Add methods to convert to and from geocentric spherical to geocentric
    Cartesian coordinates to the `Ellipsoid` class. Functions take a
    `coordinates` tuple and performs the conversions. Cartesian coordinates
    are returned in meters.

--- src/boule/_ellipsoid.py
+++ src/boule/_ellipsoid.py
@@ -150,6 +150,9 @@ class Ellipsoid:
     reference = attr.ib(default=None)
     comments = attr.ib(default=None)
 
+    # Attribute validation
+    # ##################################################################################
+
     @flattening.validator
     def _check_flattening(self, flattening, value):  # noqa: ARG002
         """
@@ -192,6 +195,9 @@ class Ellipsoid:
         if value < 0:
             message = f"The geocentric gravitational constant is negative: '{value}'"
             warn(message, stacklevel=2)
+
+    # Properties
+    # ##################################################################################
 
     @property
     def semiminor_axis(self):
@@ -500,6 +506,9 @@ class Ellipsoid:
                 subsequent_indent=2 * " ",
             )
         return s
+
+    # Coordinates and conversions
+    # ##################################################################################
 
     def geocentric_radius(self, latitude, coordinate_system="geodetic"):
         r"""
@@ -829,6 +838,78 @@ class Ellipsoid:
         )
 
         return longitude, np.degrees(latitude), height
+
+    def spherical_to_cartesian(self, coordinates):
+        """
+        Convert from spherical coordinates to geocentric Cartesian coordinates.
+
+        In the geocentric Cartesian system, z is aligned with the Earth's axis
+        of rotation and points towards the North pole, x and y are contained in
+        the equatorial plane, x coincides with the Greenwich meridian, and
+        y completes right-handed coordinate system.
+
+        Parameters
+        ----------
+        coordinates : tuple = (longitude, latitude_spherical, height)
+            Longitude, latitude, and radius coordinates of the points in
+            a geocentric spherical coordinate system. Each element can be
+            a single number or an array. The shape of the arrays must be
+            compatible. Longitude and latitude must be in degrees and radius in
+            meters.
+
+        Returns
+        -------
+        converted_coordinates : tuple = (x, y, z)
+            The converted x, y, z coordinates in a geocentric Cartesian
+            coordinate system. The shape of each element will be compatible
+            with the shape of the inputs. All coordinates are in meters.
+        """
+        longitude, latitude, radius = coordinates
+        latitude_radians = np.radians(latitude)
+        longitude_radians = np.radians(longitude)
+        sinlat = np.sin(latitude_radians)
+        coslat = np.cos(latitude_radians)
+        sinlon = np.sin(longitude_radians)
+        coslon = np.cos(longitude_radians)
+        x = radius * coslat * coslon
+        y = radius * coslat * sinlon
+        z = radius * sinlat
+        return x, y, z
+
+    def cartesian_to_spherical(self, coordinates):
+        """
+        Convert from geocentric Cartesian coordinates to spherical coordinates.
+
+        In the geocentric Cartesian system, z is aligned with the Earth's axis
+        of rotation and points towards the North pole, x and y are contained in
+        the equatorial plane, x coincides with the Greenwich meridian, and
+        y completes right-handed coordinate system.
+
+        Parameters
+        ----------
+        coordinates : tuple = (x, y, z)
+            The x, y, z coordinates of the points in a geocentric Cartesian coordinate
+            system. Each element can be a single number or an array. The shape of the
+            arrays must be compatible. All coordinates must be in meters.
+
+        Returns
+        -------
+        converted_coordinates : tuple = (longitude, latitude_spherical, radius)
+            The converted longitude, spherical latitude, and radius coordinates in
+            a geocentric spherical coordinate system. The shape of each element will
+            be compatible with the shape of the inputs. Longitude and latitude are in
+            degrees and radius in meters
+        """
+        # Make sure everything is a float to avoid overflows in the radius calculation
+        # if things are integers (happens on windows sometimes).
+        x, y, z = (np.float64(c) for c in coordinates)
+        radius = np.sqrt(x**2 + y**2 + z**2)
+        latitude = np.degrees(np.arcsin(z / radius))
+        longitude = np.degrees(np.arctan2(y, x))
+        return longitude, latitude, radius
+
+    # Gravity
+    # ##################################################################################
 
     def normal_gravity(self, coordinates, si_units=False):
         r"""
